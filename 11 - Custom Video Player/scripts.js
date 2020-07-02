640ee6583dbf18f9192bad0340a68283469da852
Leila Skinner <beileiwang@gmail.com>
2020-07-02 17:18:22 -0500

    added tiggle full screen

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -10,6 +10,8 @@ const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
+const view = player.querySelector(".screenSize");
+
 // build out functions
 function togglePlay() {
   // pause is a property for video
@@ -53,6 +55,13 @@ function scrub(event) {
   const scrubTime = (event.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
 }
+
+function toggleScreenView() {
+  console.log("Screen Size Button Clicked");
+  player.classList.toggle("fullScreen");
+  const iconButton = player.classList.contains("fullScreen") ? "⇲" : "⇱";
+  view.textContent = iconButton;
+}
 // hook up the event listeners
 // play and stop video when click on the video
 video.addEventListener("click", togglePlay);
@@ -73,3 +82,5 @@ progress.addEventListener("click", scrub);
 progress.addEventListener("mousemove", (event) => mousedown && scrub(event));
 progress.addEventListener("mousedown", () => (mousedown = true));
 progress.addEventListener("mouseup", () => (mousedown = false));
+
+view.addEventListener("click", toggleScreenView);
